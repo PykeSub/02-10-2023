acdc1df2f2106695d93526cbe546cbbd58afc36e
PykeSub <132478713+PykeSub@users.noreply.github.com>
2023-10-11 09:23:12 -0300

    Update main.py

--- Ejercicio 3/main.py
+++ Ejercicio 3/main.py
@@ -2,10 +2,11 @@ from Rectangulo import Rectangulo
 from Cubo import Cubo
 
 r = Rectangulo(7, 9)
-print(r.perimetro())
+print(r.Perimetro())
+print(r.Area())
 r.ver()
 lado3 = input('Ingrese lado3 : ')
 c = Cubo(4, 4, 4)
 #c = Cubo(4)
-print(c.volumen())
+print(c.Volumen())
 c.ver()
