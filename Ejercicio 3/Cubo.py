7c06d4c9efab29b39769a8577dee5222b70f2904
PykeSub <132478713+PykeSub@users.noreply.github.com>
2023-10-11 09:07:26 -0300

    Update Cubo.py

--- Ejercicio 3/Cubo.py
+++ Ejercicio 3/Cubo.py
@@ -3,17 +3,19 @@ from Rectangulo import Rectangulo
 class Cubo(Rectangulo):
     def __init__(self, largo: float, ancho: float, lado3: float):
         super().__init__(largo, ancho)
-        self.lado3 = lado3
+    #def __init__(self, lado3: float):
+        #super().__init__(lado3)
+        self.__lado3 = lado3
         
     def volumen(self):
-        return self.largo * self.ancho * self.lado3
+        v = self.lado3 ** 3
+        #v = super().area() * self.__lado3
+        #v = super().get__ancho() * super().get__largo() * self.__lado3
     
     def ver(self):
-        super().ver()
-        print(f'Lado 3: {self.lado3}')
+        print(f'Lado 3: {self.__lado3}')
         print(f'Volumen: {self.volumen()}')
-        
-    def __str__(self):
-        return f'Cubo con lados {self.largo}, {self.ancho}, {self.lado3}'
-    
+        print(f'Perimetro: {self.Perimetro()}')
+        print(f'Area: {self.Area()}')
+
     
